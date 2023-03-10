44a87c59b702d3c408bb5699c0c5432bc5902c1e
Reptartheman <117610334+Reptartheman@users.noreply.github.com>
2023-03-09 20:00:00 -0500

    push with new code

--- server.js
+++ server.js
@@ -6,3 +6,18 @@ const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
