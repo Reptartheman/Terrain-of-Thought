b04fd8f5a4d9b045d3983f861f6f1853c6bf8647
carsonadair <carsonadairphoto@gmail.com>
2023-03-15 18:58:05 -0400

    update test2 and add session

--- server.js
+++ server.js
@@ -3,15 +3,30 @@ const path = require('path');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
+const session = require('express-session');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create({});
 
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: ' ',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
-
-
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
