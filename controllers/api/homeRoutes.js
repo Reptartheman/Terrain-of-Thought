23d349d4cdff406a64be38e840fdbbbf3215189d
Reptartheman <117610334+Reptartheman@users.noreply.github.com>
2023-03-16 18:26:01 -0400

    started setting up the routes

--- controllers/api/homeRoutes.js
+++ controllers/api/homeRoutes.js
@@ -1,16 +1,27 @@
 const router = require('express').Router();
-const { User, Park } = require('../../models');
+const { Journal, Park } = require('../../models');
 
+//find all parks and render to homepage
  router.get('/homepage', async (req, res) => {
     try{
-        const parkData = await Park.findAll({
-            include: ['name', 'state', 'activities']
-        });
+        const parkData = await Park.findAll({})
+        const parks = parkData.map((park) => park.get({ plain: true}))
         
-        
-
-        res.render('homepage');
+        res.render('homepage', { layout: 'main', parks});
     } catch (err){
         res.status(500).json(err);
     }
 }); 
+
+
+
+router.get('/journal', async(req, res) => {
+    try{
+        const journalData = await Journal.findAll({});
+        const entries = journalData.map((entry) => entry.get({plain: true}))
+        res.render('journal')
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
