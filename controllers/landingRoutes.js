efda96b27df13d1080a8cbab08be2e1191265b88
alylmeier <alylmeier@gmail.com>
2023-03-16 16:00:46 -0400

    landing page and create user route working

--- controllers/landingRoutes.js
+++ controllers/landingRoutes.js
@@ -1,23 +1,22 @@
 // This page will retrieve all of our data for the home page...GET
-const router = require('express').Router();
-
-
-
-router.get('/', async (req, res) => {
-res.render('landing')
+const router = require("express").Router();
+const withAuth = require("../utils/auth")
+router.get("/", async (req, res) => {
+  console.log()
+  res.render("landing", { logged_in: req.session.logged_in });
 });
 
-
-router.get('/login', async (req, res) => {
-  if(req.session.logged_in) {
-    res.redirect('homepage')
+router.get("/login", async (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect("homepage");
     return;
   }
-  res.render('login')
+  res.render("login");
 });
 
-
-
-
+router.get("/mydashboard",withAuth, (req, res) => {
+ res.render('journal')
+  //const userData = await User.findone(where: {id:req.session.user_id})
+})
 
 module.exports = router;
