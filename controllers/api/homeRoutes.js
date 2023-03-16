18a5610c2fe1f561d56d120af1207cb47678bec5
carsonadair <carsonadairphoto@gmail.com>
2023-03-16 19:22:59 -0400

    journal handlebar css update with map

--- controllers/api/homeRoutes.js
+++ controllers/api/homeRoutes.js
@@ -15,13 +15,25 @@ const { Journal, Park } = require('../../models');
 
 
 
-router.get('/journal', async(req, res) => {
+router.get('/journal/:id', async(req, res) => {
     try{
         const journalData = await Journal.findAll({});
         const entries = journalData.map((entry) => entry.get({plain: true}))
-        res.render('journal')
+        // Sending park data to handlebars
+        const parkData = await Park.findByPk(req.params.id);
+        const park = parkData.get({ plain: true });
+        res.render('journal', {
+            park,
+            logged_in: req.session.logged_in})
     } catch (err) {
         res.status(500).json(err)
     }
 })
 
+// router.post('/journal/:id', async (req, res) => {
+// 
+//})
+
+// router.put('/journal/:id', async (req, res) => {
+// 
+//})
