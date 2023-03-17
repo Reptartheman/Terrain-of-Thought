8ad62a5ca43b3dcc9649c73019b0d8b6b142a8f5
carsonadair <carsonadairphoto@gmail.com>
2023-03-16 21:26:33 -0400

    grid and submit text check

--- controllers/api/journalRoute.js
+++ controllers/api/journalRoute.js
@@ -2,10 +2,33 @@ const router = require('express').Router();
 const { Journal, User } = require('../../models');
 
 
-router.post('/journal', async (req, res) => {
+router.get('/journal/:id', async(req, res) => {
+    try{
+        const journalData = await Journal.findAll({});
+        const entries = journalData.map((entry) => entry.get({plain: true}))
+        // Sending park data to handlebars
+        const parkData = await Park.findByPk(req.params.id);
+        const park = parkData.get({ plain: true });
+        res.render('journal', {
+            park,
+            logged_in: req.session.logged_in})
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+// router.post('/journal/:id', async (req, res) => {
+// 
+//})
+
+// router.put('/journal/:id', async (req, res) => {
+// 
+//})
+
+router.post('/journal/:id', async (req, res) => {
     try {
         const newEntry = await Journal.create({
-            ...req.body
+            ...req.body, park_id: req.params.id
         })
         res.json(newEntry)
     } catch (err) {
--- public/js/script.js
+++ public/js/script.js
@@ -24,12 +24,13 @@ function choosePic() {
 
 // Leaflet map functions
 
-var map = L.map('map').setView([51.505, -0.09], 13);
-var marker = L.marker([51.5, -0.09]).addTo(map);
-marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+// var map = L.map('map').setView([51.505, -0.09], 13);
+// var marker = L.marker([51.5, -0.09]).addTo(map);
+// marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
 
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+// L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//     maxZoom: 19,
+//     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+// }).addTo(map);
+
