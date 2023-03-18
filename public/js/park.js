ffa2ae4db651c423e1dd78946bcfaddc78c33a2d
carsonadair <carsonadairphoto@gmail.com>
2023-03-18 13:16:46 -0400

    didnt fix the journals but did fix the css

--- public/js/park.js
+++ public/js/park.js
@@ -1,15 +1,27 @@
-const whatever = document.querySelectorAll('.park-image')
+const parkIcon = document.querySelectorAll('.park-image')
 
 const newHandler = async (e) => {
     e.preventDefault();
     console.log(e.target);
 
+    if ( something ) {
+      const response = await fetch(`/api/journalRoute`, {
+        method: 'GET',
+      });
+  
+      if (response.ok) {
+        document.location.replace('/journal/:id');
+      } else {
+        alert('');
+      }
+    }
+  };
+  
+
     // const park = document.e.target.value.trim();
 
-}
 
-
-for (i of whatever) {
+for (i of parkIcon) {
   i.addEventListener('click', function() {
     console.log(this);
   });
